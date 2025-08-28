b742ef03f84dec4a4ae9b28b4046a3ee04918f95
allanbavajee <allan.bavajee@gmail.com>
2025-08-28 12:22:34 +0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+/*fintrack/pages/index.js*/
+export default function Dashboard() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
+      <p>Bienvenue dans FinTrack, votre tableau de bord financier.</p>
+    </div>
+  );
+}
