8aaa9e2a913bf9b4dfd12852f253cbbfd4123813
allanbavajee <allan.bavajee@gmail.com>
2025-08-28 12:40:20 +0400

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-/*fintrack/pages/index.js*/
-export default function Dashboard() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
-      <p>Bienvenue dans FinTrack, votre tableau de bord financier.</p>
-    </div>
-  );
-}
