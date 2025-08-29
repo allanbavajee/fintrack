570582a583eb0b5edacdbbc0777542edc44bfc03
allanbavajee <allan.bavajee@gmail.com>
2025-08-29 14:52:21 +0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+/* fintrack/pages/index.js */
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
+      <h1>Bienvenue sur Fintrack 🚀</h1>
+      <p>Déploiement réussi ✅</p>
+    </div>
+  );
+}
