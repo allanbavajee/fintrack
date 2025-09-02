89837ad494aa0d7325a7af0e3ec0f91cdfcb8619
allanbavajee <allan.bavajee@gmail.com>
2025-09-02 16:03:33 +0400

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -16,29 +16,17 @@ export default function Navbar() {
     >
       {/* Logo + Slogan */}
       <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
-        <Image
-          src="/images/fintrack.logo.png"
-          alt="Fintrack Logo"
-          width={120}
-          height={50}
-          priority
-        />
-        <span style={{ fontSize: "14px", color: "#555", marginTop: "4px" }}>
-          Votre partenaire financier
+        <div style={{ display: "flex", alignItems: "center", gap: 12 }}>
+          <Image src="/images/fintrack.logo.png" alt="Fintrack Logo" width={120} height={50} priority />
+        </div>
+        <span style={{ fontSize: 14, color: "#555", marginTop: 4 }}>
+          Your Finances, Your Way.
         </span>
       </div>
 
       {/* Menu */}
       <nav>
-        <ul
-          style={{
-            display: "flex",
-            listStyle: "none",
-            gap: "24px",
-            margin: 0,
-            padding: 0,
-          }}
-        >
+        <ul style={{ display: "flex", listStyle: "none", gap: 24, margin: 0, padding: 0 }}>
           <li><Link href="/">Accueil</Link></li>
           <li><Link href="/clients">Clients</Link></li>
           <li><Link href="/invoices">Invoices</Link></li>
