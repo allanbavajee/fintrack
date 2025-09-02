0335cf9ba551ffb15415b7210b00045219083097
allanbavajee <allan.bavajee@gmail.com>
2025-09-02 16:03:14 +0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 /* pages/_app.jsx */
-import Navbar from "../components/Navbar";
 import "../styles/globals.css";
+import Navbar from "../components/Navbar";
 
 export default function MyApp({ Component, pageProps }) {
   return (
