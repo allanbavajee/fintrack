47afb72abababc0051e7aca9426fa19cc8170b2b
allanbavajee <allan.bavajee@gmail.com>
2025-09-03 11:55:35 +0400

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,42 +4,22 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <header
-      style={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        padding: "16px 32px",
-        background: "#fff",
-        borderBottom: "1px solid #ddd",
-        position: "relative",
-      }}
-    >
-      {/* Logo + slogan à gauche */}
+    <header style={{ display: "flex", alignItems: "center", justifyContent: "space-between", padding: "16px 32px", background: "#fff", borderBottom: "1px solid #ddd" }}>
+      
+      {/* Logo + slogan */}
       <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
-        <Image src="/images/fintrack.logo.png" alt="Fintrack Logo" width={120} height={50} style={{ objectFit: "contain" }} />
+        <Image src="/images/fintrack.logo.png" alt="Fintrack Logo" width={120} height={50} />
         <span style={{ fontSize: 14, color: "#555", marginTop: 4 }}>
           Your money, your way.
         </span>
       </div>
 
-      {/* Menu à gauche du logo */}
-      <nav style={{ display: "flex", gap: 24, marginLeft: 32 }}>
+      {/* Menu */}
+      <nav style={{ display: "flex", gap: 24 }}>
         <Link href="/">Home</Link>
         <Link href="/about-us">About Us</Link>
         <Link href="/contact">Contact Us</Link>
       </nav>
-
-      {/* Login / Signup en haut à droite */}
-      <div style={{ position: "absolute", right: 32, top: 16 }}>
-        <Link href="/login">
-          <button style={{ marginRight: 8, padding: "8px 16px", borderRadius: 8, border: "1px solid #0ea5a0", background: "#fff", color: "#0ea5a0", cursor: "pointer" }}>Login</button>
-        </Link>
-        <Link href="/signup">
-          <button style={{ padding: "8px 16px", borderRadius: 8, border: "none", background: "#0ea5a0", color: "#fff", cursor: "pointer" }}>Signup</button>
-        </Link>
-      </div>
     </header>
   );
 }
-
